687bcdb45ca2fb0acb64e03067465dafeac0791d
ggranleese <george.granleese@cmail.carleton.ca>
2018-11-29 01:55:55 -0500

    REFACTOR: Modified resources implimentation

--- main/core/RummikubView.java
+++ main/core/RummikubView.java
@@ -1,13 +1,27 @@
 package core;
 
+
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+import javax.imageio.stream.FileImageInputStream;
+
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.layout.Pane;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.control.Button;
 
 public class RummikubView {
 	
 	private RummikubController controller;
 	private RummikubModel model;
 	private Pane view;
+
 	
 
 	public RummikubView(RummikubController controller, RummikubModel model) {
@@ -22,11 +36,23 @@ public class RummikubView {
 		return view;
 	}
 	
+	@SuppressWarnings("restriction")
 	public void initView() {
 		view = new Pane();
 		view.setStyle("-fx-background-color: green");
 		
+		ImageView image = new ImageView(new Image("/Tiles/b1.jpg"));
+		
+		
+		Button button = new Button("TEST");
+		//loadDeck();
+		
+		
+		view.getChildren().addAll(button);
+
+		
 	}
+
 
 	
 }
