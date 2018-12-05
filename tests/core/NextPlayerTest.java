d95d07aa1e9fe6472a9128f32a22a2dc6ca6d257
Lagawaga <kamronjahani@gmail.com>
2018-12-05 08:46:19 -0500

    Code: fixed issue with random card being dealt at the end of players turn (no melds played)

--- tests/core/NextPlayerTest.java
+++ tests/core/NextPlayerTest.java
@@ -4,16 +4,19 @@ import java.util.ArrayList;
 
 import junit.framework.TestCase;
 import model.Player;
+import model.RummikubModel;
 import view.RummikubView;
 
 public class NextPlayerTest extends TestCase{
 	public void test() {
 		RummikubView view = new RummikubView();
+		RummikubModel model = new RummikubModel();
+		RummikubController controller = new RummikubController(model);
 		ArrayList<Player> players = new ArrayList<Player>();
 		players.add(new Player());
 		players.add(new Player());
-		view.currentPlayer = view.model.getPlayers().get(0);
-		ArrayList<Player> modelplayers = view.model.getPlayers();
+		view.currentPlayer = controller.model.getPlayers().get(0);
+		ArrayList<Player> modelplayers = controller.model.getPlayers();
 		modelplayers = players;
 		view.nextPlayerTurn();
 		
